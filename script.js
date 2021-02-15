d02cd8b5bb7e4768b36b26c9b53924be9aa4948d
Samkiroko <kirokodev@gmail.com>
2021-02-15 14:57:44 +0300

    Introduction to functions

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 'use strict';
-let hasDriversLicense = false;
-const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('i can drive');
+function logger() {
+  console.log('My name is samuel kiroko');
+}
+// calling /running /invoking function
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 2);
+console.log(appleJuice);
+
+const appleJuice2 = fruitProcessor(10, 12);
+alert(appleJuice2);
