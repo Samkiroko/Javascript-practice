a29ce08030605e0c1d69d31aed5ab35aa39e4885
Samkiroko <kirokodev@gmail.com>
2021-02-15 20:09:06 +0300

    function to function

--- script.js
+++ script.js
@@ -31,16 +31,48 @@
 // const age2 = calAge2(1993);
 // console.log(age1, age2);
 
-/* Arrow functions*/
-const calAge3 = (birthYear) => 2020 - birthYear;
-const age3 = calAge3(1989);
-console.log(age3);
+// /* Arrow functions*/
+// const calAge3 = (birthYear) => 2020 - birthYear;
+// const age3 = calAge3(1989);
+// console.log(age3);
 
-// year till retirement
-const yearUntilRetirement = (birthYear, firstName) => {
-  const age = 2020 - birthYear;
-  const retirement = 65 - age;
-  // return retirement;
-  return `${firstName} retires in ${retirement} years`;
+// // year till retirement
+// const yearUntilRetirement = (birthYear, firstName) => {
+//   const age = 2020 - birthYear;
+//   const retirement = 65 - age;
+//   // return retirement;
+//   return `${firstName} retires in ${retirement} years`;
+// };
+// console.log(yearUntilRetirement(1985, 'Joyce'));
+
+/*Function calling Other functions*/
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+  const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+
+const calAge = function (birthYear) {
+  return 2020 - birthYear;
 };
-console.log(yearUntilRetirement(1985, 'Joyce'));
+
+const yearUntilRetirement = function (birthYear, firstName) {
+  const age = calAge(birthYear);
+  const retirement = 65 - age;
+  if (retirement > 0) {
+    return `${firstName} retires in ${retirement} years`;
+  } else {
+    return `${firstName} you are already retired`;
+  }
+};
+
+console.log(yearUntilRetirement(1989, 'Samuel'));
+console.log(yearUntilRetirement(1889, 'Sam'));
