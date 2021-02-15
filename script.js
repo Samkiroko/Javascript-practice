f716094e28e69a0ba6c8735eb31d55d0bcd348f3
Samkiroko <kirokodev@gmail.com>
2021-02-15 15:18:11 +0300

    function declaration & expression

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 'use strict';
 
-function logger() {
-  console.log('My name is samuel kiroko');
+// function logger() {
+//   console.log('My name is samuel kiroko');
+// }
+// // calling /running /invoking function
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 2);
+// console.log(appleJuice);
+
+// const appleJuice2 = fruitProcessor(10, 12);
+// alert(appleJuice2);
+
+/* Function declaration VS Expressions */
+function calAge1(birthYear) {
+  return 2037 - birthYear;
 }
-// calling /running /invoking function
-logger();
+const age1 = calAge1(1989);
 
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
-}
+/* Expression  */
 
-const appleJuice = fruitProcessor(5, 2);
-console.log(appleJuice);
-
-const appleJuice2 = fruitProcessor(10, 12);
-alert(appleJuice2);
+const calAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calAge2(1993);
+console.log(age1, age2);
