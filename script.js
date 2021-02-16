eff3fa758e3d2fe9fb662afe5164c703ab1a63fa
Samkiroko <kirokodev@gmail.com>
2021-02-16 08:34:44 +0300

    function to function

--- script.js
+++ script.js
@@ -78,22 +78,62 @@
 // console.log(yearUntilRetirement(1889, 'Sam'));
 
 /*Coding Challenge one*/
-const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+/* Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new 
+gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so 
+one average score per team).
+A team only wins if it has at least double the average score of the other team. 
+Otherwise, no team wins!
+Your tasks:
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team 
+as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner 
+to the console, together with the victory points, according to the rule above. 
+Example: "Koalas win (30 vs. 13)"
+4. Use the 'checkWinner' function to determine the winner for both Data 1 and 
+Data 2
+5. Ignore draws this time
+Test data:
+§ Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+§ Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+Hints:
+§ To calculate average of 3 values, add them all together and divide by 3
+§ To check if number A is at least double number B, check for A >= 2 * B. 
+Apply this to the team's average scores 
+GOOD LUCK*/
+// const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
-// test data
-const averDolphin = calcAverage(85, 54, 41);
-console.log(averDolphin);
-const averKoalas = calcAverage(23, 34, 27);
-console.log(averKoalas);
+// // test data
+// const averDolphin = calcAverage(85, 54, 41);
+// console.log(averDolphin);
+// const averKoalas = calcAverage(23, 34, 27);
+// console.log(averKoalas);
 
-function checkWinner(averDolphin, averKoalas) {
-  if (averDolphin >= 2 * averKoalas) {
-    return `Dolphins win 🏆 ${averDolphin} vs ${averKoalas}`;
-  } else if (averKoalas >= 2 * averDolphin) {
-    return `Koalas win 🏆 ${averKoalas} vs ${averDolphin}`;
-  } else {
-    return 'No one wins 🤔';
-  }
-}
+// function checkWinner(averDolphin, averKoalas) {
+//   if (averDolphin >= 2 * averKoalas) {
+//     return `Dolphins win 🏆 ${averDolphin} vs ${averKoalas}`;
+//   } else if (averKoalas >= 2 * averDolphin) {
+//     return `Koalas win 🏆 ${averKoalas} vs ${averDolphin}`;
+//   } else {
+//     return 'No one wins 🤔';
+//   }
+// }
 
-console.log(checkWinner(averDolphin, averKoalas));
+// console.log(checkWinner(averDolphin, averKoalas));
+
+/* Introduction to arrays */
+const friend1 = 'Michael';
+const friend2 = 'Milla';
+const friend3 = 'Mic';
+const friend4 = 'Mich';
+
+const friends = ['Michael', 'Milla', 'Mic', 'Mich'];
+console.log(friends);
+
+const years = new Array(2020, 2000, 1889, 1845);
+console.log(years);
+console.log(years[0]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
